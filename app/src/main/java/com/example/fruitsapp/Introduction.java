7ba8daf0b8136c83e3bae91ce28f60ac106cf723
Priyanhsu Pundhir <priyanshupundhir@gmail.com>
2021-10-21 11:34:24 +0530

    Push 9

--- app/src/main/java/com/example/fruitsapp/Introduction.java
+++ app/src/main/java/com/example/fruitsapp/Introduction.java
@@ -2,12 +2,16 @@ package com.example.fruitsapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.widget.ImageView;
+import java.util.Timer;
+import java.util.TimerTask;
 
 import com.airbnb.lottie.LottieAnimationView;
 
 public class Introduction extends AppCompatActivity {
+    Timer timer;
 
     ImageView bg;
     LottieAnimationView lottieAnimationView;
@@ -16,6 +20,16 @@ public class Introduction extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_introduction);
+
+        timer=new Timer();
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(Introduction.this,MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 3000);
 
         bg = findViewById(R.id.imgbg);
         lottieAnimationView = findViewById(R.id.animation);
